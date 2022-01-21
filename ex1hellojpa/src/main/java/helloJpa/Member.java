387ac76773183c80fc1a5fbebc235a36dd771ef1
KoHanSol <abc35572003@naver.com>
2022-01-21 19:43:38 +0900

    1.5 엔티티 매핑 1.5.1 객체와 테이블 매핑 - Entity 속성, Entity, Table 어노테이션 정리

--- ex1hellojpa/src/main/java/helloJpa/Member.java
+++ ex1hellojpa/src/main/java/helloJpa/Member.java
@@ -2,8 +2,13 @@ package helloJpa;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+//엔티티 매핑
+//JPA를 사용해서 테이블과 매핑할 클래스는 @Entity 필수
 @Entity
+//엔티티 속성 name을 이용하여 매핑 테이블 지정 가능(@Table은 엔티티와 매핑할 테이블 지정)
+@Table(name="MBR")
 public class Member {
 
     @Id
