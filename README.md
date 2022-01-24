81ad8f03f0fa3abc75325f9519fcb8c86350d762
KoHanSol <abc35572003@naver.com>
2022-01-24 22:14:08 +0900

    1.5.4 기본 키 매핑 방법 - 시퀀스 전략 특징 정리(특히 allocationSize 속성에 대해) - IDENTITY 전략 특징 정리

--- README.md
+++ README.md
@@ -210,6 +210,8 @@ member.setName("Modify Name");
 • 직접 할당: @Id만 사용
 • 자동 생성(@GeneratedValue) 
    • IDENTITY: 데이터베이스에 위임, MYSQL 
+     • JPA는 보통 트랜잭션 커밋 시점에 INSERT SQL 실행
+     • IDENTITY 전략은 em.persist() 시점에 즉시 INSERT SQL 실행하고 DB에서 식별자를 조회
    • SEQUENCE: 데이터베이스 시퀀스 오브젝트 사용, ORACLE 
      • @SequenceGenerator 필요
    • TABLE: 키 생성용 테이블 사용, 모든 DB에서 사용
--- ex1hellojpa/src/main/java/helloJpa/JpaMain2.java
+++ ex1hellojpa/src/main/java/helloJpa/JpaMain2.java
@@ -27,59 +27,34 @@ public class JpaMain2 {
 
         try{
 
-            //영속
-            //Member member1 = new Member(150L,"A");
-            //Member member2 = new Member(160L,"B");
-            //em.persist(member1);
-            //em.persist(member2);
-            //System.out.println("=================");
+            Member member1 = new Member();
+            member1.setUsername("D");
+            Member member2 = new Member();
+            member2.setUsername("E");
+            Member member3 = new Member();
+            member3.setUsername("F");
+
+            System.out.println("===========================");
+            //IDENTITY 전략은 em.persist() 시점에 즉시 INSERT SQL 실행하고 DB에서 식별자를 조회
+            //시퀀스 전략은 이떄 시퀀스 값 불러온다.
             
-            //엔티티 수정 - 변경 감지
-            //Member member = em.find(Member.class,150L);
-            //데이터 변경 후 persist 호출할 필요 없다.
-            //member.setName("Modify Name1");
+            //em.persist()를 주석 처리 하면 시퀀스를 두번 호출하는걸 확인 할 수 있따.
+            //DB_SEQ = 1 |  1
+            //DB_SEQ = 51 |  2
+            //DB_SEQ = 51 |  3
 
-            //영속성 컨텍스트를 플러시 하는 방법
-            //Member member = new Member(300L,"member300");
-            //em.persist(member);
+            em.persist(member1); //1, 51
+            em.persist(member2); // DB 실행하지않고 메모리에서 호출
+            em.persist(member3); // DB 실행하지않고 메모리에서 호출
 
-            // 쓰기 지연 SQL 저장소의 쿼리를 데이터베이스에 반영(등록, 수정, 삭제 쿼리)
-            //트랜잭션 commit과 상관없이 즉시(쿼리가 DB에 반영) 일어난다.
-            //em.flush();
-
-//            //준영속 -> 영속 상태의 엔티티가 영속성 컨텍스트에서 분리(detached)
-//           Member member = em.find(Member.class,150L);
-//            member.setName("clear!");
-//
-//            //특정 엔티티만 준영속 상태로 전환
-//            //em.detach(member);
-//
-//            //영속성 컨텍스트를 완전히 초기화
-//            em.clear();
-//
-//            //초기화 했기 때문에 다시 영속성 컨텍스트를 올린다.
-//            Member member2 = em.find(Member.class,"150L");
-//
-//            System.out.println("==================");
-//
-//            //커밋하는 순간 데이터베이스에 INSERT SQL을 보낸다.
-
-            //@Enumerated 설명
-            //ORDINAL 기본 쓰면 안되는 이유
-            //요구사항이 늘어나서 RoleType이 추가된 경우 순서가 바뀌어버린다.
-            //그래서 STRING으로 사용해야한다.
-            Member member = new Member();
-            //member.setId(4L);
-            //member.setUsername("D");
-            //member.setRoleType(RoleType.GUEST);
-
-            //기본 키 매핑 방법
-            //member.setId("ID_A");
-            member.setUsername("D");
-
-            em.persist(member);
-
+            System.out.println("member1 id = "+member1.getId());
+            System.out.println("member2 id = "+member2.getId());
+            System.out.println("member3 id = "+member3.getId());
+            System.out.println("===========================");
+            
+            //시퀀스 전략은 이떄 실질적 insert 쿼리를 실행한다.
             tx.commit();
+
         }catch (Exception e){
             tx.rollback();
         }finally {
